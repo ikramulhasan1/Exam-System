739e87f655c8a4ab05ed3051b25b3ab80afde5df
rima <rimakhan0345@gmail.com>
2023-10-10 19:10:03 +0600

    subject create page edit

--- resources/views/backend/subject/create.blade.php
+++ resources/views/backend/subject/create.blade.php
@@ -22,7 +22,7 @@
                             <input type="text" name="name" class="form-control" placeholder="Enter the subject">
 
                         </div>
-                        <div class="mb-3 d-flex flex-column">
+                        {{-- <div class="mb-3 d-flex flex-column">
                             <label class="form-label">Level</label>
 
                             <select class="form-select p-2 border ">
@@ -32,14 +32,14 @@
                                 <option value="3">Three</option>
                             </select>
 
-                        </div>
-                        <div class="mb-3 d-flex flex-column">
+                        </div> --}}
+                        {{-- <div class="mb-3 d-flex flex-column">
                             <label class="form-label">Question</label>
 
                             <input type="text" name="question" class="form-control" placeholder="Enter your question">
 
-                        </div>
-                        <div class="mb-3 row">
+                        </div>  --}}
+                        {{-- <div class="mb-3 row">
                             <div class="d-flex col-lg-6 mb-3">
                                 <input type="text" name="option1" class="form-control" placeholder="Option-1">
                             </div>
@@ -52,7 +52,7 @@
                             <div class="d-flex col-lg-6 mb-3">
                                 <input type="text" name="option4" class="form-control" placeholder="Option-4">
                             </div>
-                        </div>
+                        </div> --}}
 
                         <button type="submit" class="btn btn-primary">Submit</button>
                     </form>
@@ -91,38 +91,7 @@
                         <input type="text" name="name" class="form-control" placeholder="Enter the subject">
 
                     </div>
-                    <div class="mb-3 d-flex flex-column">
-                        <label class="form-label">Level</label>
-
-                        <select class="form-select p-2 border ">
-                            <option selected>select menu</option>
-                            <option value="1">One</option>
-                            <option value="2">Two</option>
-                            <option value="3">Three</option>
-                        </select>
-
-                    </div>
-                    <div class="mb-3 d-flex flex-column">
-                        <label class="form-label">Question</label>
-
-                        <input type="text" name="question" class="form-control" placeholder="Enter your question">
-
-                    </div>
-                    <div class="mb-3 row">
-                        <div class="d-flex col-lg-6 mb-3">
-                            <input type="text" name="option1" class="form-control" placeholder="Option-1">
-                        </div>
-                        <div class="d-flex col-lg-6 mb-3">
-                            <input type="text" name="option2" class="form-control" placeholder="Option-2">
-                        </div>
-                        <div class="d-flex col-lg-6 mb-3">
-                            <input type="text" name="option3" class="form-control" placeholder="Option-3">
-                        </div>
-                        <div class="d-flex col-lg-6 mb-3">
-                            <input type="text" name="option4" class="form-control" placeholder="Option-4">
-                        </div>
-                    </div>
-
+                    
                     <button type="submit" class="btn btn-primary">Submit</button>
                 </form>
             </div>
